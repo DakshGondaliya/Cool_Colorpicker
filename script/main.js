672cd90c718006ec22977dffe34d9ea172b3ef00
DakshGondaliya <gondaliyadaksh@gmail.com>
2019-01-08 14:59:23 +0530

    Added logo

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 // When the user scrolls down 30px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function () {
+  growShrinkLogo();
   scrollFunction();
 };
 
@@ -8,10 +9,19 @@ function scrollFunction() {
     document.body.scrollTop > 60 ||
     document.documentElement.scrollTop > 60
   ) {
-    document.getElementById("navbar").style.padding = "15px 5px";
-    document.getElementById("logo").style.fontSize = "30px";
+    document.getElementById("navbar").style.padding = "5px 5px";
+    // document.getElementById("logo").style.fontSize = "30px";
   } else {
-    document.getElementById("navbar").style.padding = "20px 5px";
-    document.getElementById("logo").style.fontSize = "40px";
+    document.getElementById("navbar").style.padding = "10px 5px";
+    // document.getElementById("logo").style.fontSize = "40px";
+  }
+}
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("logo")
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    Logo.style.width = '200px';
+  } else {
+    Logo.style.width = '250px';
   }
 }
