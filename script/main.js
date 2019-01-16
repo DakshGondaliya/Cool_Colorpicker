7ca42cbfc17d8e4e29537180cd12bca9099211cb
DakshGondaliya <gondaliyadaksh@gmail.com>
2019-01-16 17:19:44 +0530

    Added media query

--- script/main.js
+++ script/main.js
@@ -1,15 +1,12 @@
 // When the user scrolls down 30px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function () {
+window.onscroll = function() {
   growShrinkLogo();
   scrollFunction();
   scrollindicator();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 40 ||
-    document.documentElement.scrollTop > 40
-  ) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.getElementById("navbar").style.padding = "0 2px";
     // document.getElementById("logo").style.fontSize = "30px";
   } else {
@@ -20,17 +17,19 @@ function scrollFunction() {
 }
 
 function growShrinkLogo() {
-  var Logo = document.getElementById("logo")
+  var Logo = document.getElementById("logo");
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    Logo.style.width = '200px';
+    Logo.style.width = "200px";
   } else {
-    Logo.style.width = '250px';
+    Logo.style.width = "250px";
   }
 }
 
 function scrollindicator() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
